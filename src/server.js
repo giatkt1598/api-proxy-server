6a070a52a1a7d41fed47c49e80e55cb9b1ef6d83
GiaTK <giatkt1598@gmail.com>
2025-09-22 20:59:59 +0700

    update

--- src/server.js
+++ src/server.js
@@ -13,50 +13,47 @@ const config = getConfig();
  * @param {string} pathRewrite 
  */
 function runServer(port, pathRewrite = '/') {
+    const app = express()
+        .use(cors({
+            origin: true,
+            credentials: true,
+        }))
+        .use(
+            '/',
+            createProxyMiddleware({
+                target: config.originUrl,
+                changeOrigin: true,
+                secure: false,
+                headers: config.headers,
+                cookieDomainRewrite: 'localhost',
+                on: {
+                    proxyRes: (proxyRes, req, res) => {
+                        for (let i = 0; i < proxyRes.headers['set-cookie']?.length ?? []; i++) {
+                            let cookie = proxyRes.headers['set-cookie'][i]
+                                .replace(/samesite\s*=\s*(lax|strict|none)/i, 'SameSite=None')
+                                .replace(/;\s*Secure/gi, "");
+                            proxyRes.headers['set-cookie'][i] = cookie;
+                        }
 
-    const app = express();
-    app.use(cors({
-        origin: true,
-        credentials: true,
-    }))
-
-    app.use(
-        '/',
-        createProxyMiddleware({
-            target: config.originUrl,
-            changeOrigin: true,
-            secure: false,
-            headers: config.headers,
-            cookieDomainRewrite: 'localhost',
-            on: {
-                proxyRes: (proxyRes, req, res) => {
-                    for (let i = 0; i < proxyRes.headers['set-cookie']?.length ?? []; i++) {
-                        let cookie = proxyRes.headers['set-cookie'][i]
-                            .replace(/samesite\s*=\s*(lax|strict|none)/i, 'SameSite=None')
-                            .replace(/;\s*Secure/gi, "");
-                        proxyRes.headers['set-cookie'][i] = cookie;
+                        for (const headerKey of Object.keys(config.responseHeaders)) {
+                            proxyRes.headers[headerKey] = config.responseHeaders[headerKey];
+                        }
                     }
-
-                    for (const headerKey of Object.keys(config.responseHeaders)) {
-                        proxyRes.headers[headerKey] = config.responseHeaders[headerKey];
-                    }
-                }
-            },
-            pathRewrite: (reqPath) => path.posix.join(pathRewrite, reqPath),
-        })
-    )
-
+                },
+                pathRewrite: (reqPath) => path.posix.join(pathRewrite, reqPath),
+            })
+        );
 
     const notifyServerStarted = () => {
         console.log(`Proxy server listening at ${config.ssl ? 'https' : 'http'}://localhost:${port} -> ${config.originUrl}${pathRewrite === '/' ? '' : pathRewrite}`);
     }
 
     if (config.ssl) {
-        const privateKey = config.ssl ? readFileSync("./ssl/localhost.key", "utf8") : '';
-        const certificate = config.ssl ? readFileSync("./ssl/localhost.crt", "utf8") : '';
+        const privateKey = readFileSync("./ssl/localhost.key", "utf8");
+        const certificate = readFileSync("./ssl/localhost.crt", "utf8");
         const server = createServer({
-            key: config.ssl ? privateKey : undefined,
-            cert: config.ssl ? certificate : undefined,
+            key: privateKey,
+            cert: certificate,
         }, app);
         server.listen(port, notifyServerStarted);
     } else {
